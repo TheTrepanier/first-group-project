cb805878c280593e3629fc3044c81304e63873d3
reiko321 <reiko321@gmail.com>
2019-04-02 18:03:22 -0700

    testing db

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,10 +1,11 @@
+// Initialize Firebase
 var config = {
-    apiKey: "AIzaSyAb77bSsgNLQdR8q82cobSaV2Rhjg00hhk",
-    authDomain: "project-1-60d80.firebaseapp.com",
-    databaseURL: "https://project-1-60d80.firebaseio.com",
-    projectId: "project-1-60d80",
-    storageBucket: "project-1-60d80.appspot.com",
-    messagingSenderId: "600169268617"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyCixi5L8akbPhmysRlZHIYDhwy-IXkmUmw",
+  authDomain: "first-project-fdfbd.firebaseapp.com",
+  databaseURL: "https://first-project-fdfbd.firebaseio.com",
+  projectId: "first-project-fdfbd",
+  storageBucket: "first-project-fdfbd.appspot.com",
+  messagingSenderId: "619783799705"
+};
+firebase.initializeApp(config);
   var database = firebase.database();
