c8a2aa988738bac797e6bb18627cbe91cd14a8f9
Blair Brownlie <bbrownli@asu.edu>
2019-04-02 20:58:21 -0700

    getting basic ajax calls for the ticketmaster api console logged

--- assets/javascript/ticketmaster.js
+++ assets/javascript/ticketmaster.js
@@ -1,14 +1,17 @@
-$(function(){
-    var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?"
-    var search = "&keyword=comerica+theatre"
-    var apiKey = "&apikey=uSxl2gTHIkk0ZOlvcFjRxmWklVwyB41K"
-    $.ajax({
-        url: queryURL + search + apiKey,
-        method: "GET"
-    }).then(function(response){
-        var eventList = response._embedded.events;
-
-        console.log(eventList);
-        
-    })
-})
+$(function() {
+  var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?";
+  var search = "&keyword=comerica+theatre";
+  var apiKey = "&apikey=uSxl2gTHIkk0ZOlvcFjRxmWklVwyB41K";
+  $.ajax({
+    url: queryURL + search + apiKey,
+    method: "GET"
+  }).then(function(response) {
+    var eventList = response._embedded.events;
+    console.log(eventList);
+    for (let i = 0; i < eventList.length; i++) {
+      console.log(eventList[i].name);
+      console.log(eventList[i].url);
+      console.log(eventList[i]._embedded.venues[0].name);
+    }
+  });
+});
