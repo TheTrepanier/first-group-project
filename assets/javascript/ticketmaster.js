07a77a569b2a776c0707d4160d9ed1715c5ad0c8
bigez <rjones26@outlook.com>
2019-04-04 21:16:15 -0700

    add branch

--- assets/javascript/ticketmaster.js
+++ assets/javascript/ticketmaster.js
@@ -1,17 +1,19 @@
-var ticketMasterCall = function() {
+var ticketMasterCall = function(eventSearch) {
   var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?";
-  var search = "&keyword=comerica+theatre";
+  var search = "&keyword=" + eventSearch;
   var apiKey = "&apikey=uSxl2gTHIkk0ZOlvcFjRxmWklVwyB41K";
   $.ajax({
     url: queryURL + search + apiKey,
     method: "GET"
   }).then(function(response) {
     var eventList = response._embedded.events;
-    console.log(eventList);
+    var eventName = eventList
     for (let i = 0; i < eventList.length; i++) {
+      var yelpCardTitle = $("<h2>").addClass("uk-card-title").text(name);
+
       console.log(eventList[i].name);
       console.log(eventList[i].url);
-      console.log(eventList[i]._embedded.venues[0].name);
+      // console.log(eventList[i]._embedded.venues[0].name);
     }
   });
 };
