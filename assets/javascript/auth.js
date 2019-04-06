49ce20faf2aff62eb7479ec376455a2ae2885c8a
reiko321 <reiko321@gmail.com>
2019-04-06 12:03:43 -0700

    pushing

--- assets/javascript/auth.js
+++ assets/javascript/auth.js
@@ -1,16 +1,62 @@
 var provider = new firebase.auth.GithubAuthProvider();
 
- // FirebaseUI config.
- var uiConfig = {
-    signInSuccessUrl: href="https://thetrepanier.github.io/first-group-project/",
+// FirebaseUI config.
+var uiConfig = {
+    signInSuccessUrl: href = "https://thetrepanier.github.io/first-group-project/",
     signInOptions: [
-      // Leave the lines as is for the providers you want to offer your users.
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
     ],
-  };
+};
 
-  // Initialize the FirebaseUI Widget using Firebase.
-  var ui = new firebaseui.auth.AuthUI(firebase.auth());
-  // The start method will wait until the DOM is loaded.
-  ui.start('#firebaseui-auth-container', uiConfig);
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig);
+
+// This is only relevant for single page apps
+if (ui.isPendingRedirect()) {
+    ui.start('#firebaseui-auth-container', uiConfig);
+}
+
+//   Here is how you would track the Auth state across all your pages.
+initApp = function () {
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var uid = user.uid;
+            var phoneNumber = user.phoneNumber;
+            var providerData = user.providerData;
+            user.getIdToken().then(function (accessToken) {
+                document.getElementById('sign-in-status').textContent = 'Signed in';
+                document.getElementById('sign-in').textContent = 'Sign out';
+                document.getElementById('account-details').textContent = JSON.stringify({
+                    displayName: displayName,
+                    email: email,
+                    emailVerified: emailVerified,
+                    phoneNumber: phoneNumber,
+                    photoURL: photoURL,
+                    uid: uid,
+                    accessToken: accessToken,
+                    providerData: providerData
+                }, null, '  ');
+            });
+        } else {
+            // User is signed out.
+            document.getElementById('sign-in-status').textContent = 'Signed out';
+            document.getElementById('sign-in').textContent = 'Sign in';
+            document.getElementById('account-details').textContent = 'null';
+        }
+    }, function (error) {
+        console.log(error);
+    });
+};
+
+window.addEventListener('load', function () {
+    initApp()
+});
