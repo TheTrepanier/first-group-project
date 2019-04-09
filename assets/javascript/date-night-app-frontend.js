a4cdf42ad48daa4ace123a3b420ae7e49ccc993f
TheTrepanier <thetrepanier1@Michaels-MacBook-Pro-3.local>
2019-04-08 20:46:31 -0700

    buttons are now created based on previous searches, clicking on them will do that same search

--- assets/javascript/date-night-app-frontend.js
+++ assets/javascript/date-night-app-frontend.js
@@ -25,7 +25,8 @@ var moreSpecificYelpCallButton = $("<button>").attr("id", "moreSpecificYelpCallB
 
 var offCanvasFormDiv = $("<div>").addClass("uk-margin").append(locationLabel, locationInputDiv, locationEventLabel, locationEventInputDiv);
 var offCanvasForm = $("<form>").addClass("uk-form-stacked").append(offCanvasFormDiv);
-var offCanvasDiv = $("<div>").addClass("uk-offcanvas-bar").html("<button class=\"uk-offcanvas-close\" type=\"button\" uk-close></button>").append(offCanvasTitle, hr, offCanvasForm, moreSpecificYelpCallButton);
+var offCanvasButtonDiv = $("<div>");
+var offCanvasDiv = $("<div>").addClass("uk-offcanvas-bar").attr("id", "off-canvas-div").html("<button class=\"uk-offcanvas-close\" type=\"button\" uk-close></button>").append(offCanvasTitle, hr, offCanvasForm, moreSpecificYelpCallButton, offCanvasButtonDiv);
 var offCanvasPage = $("<div>").attr("id", "offcanvas-usage").attr("uk-offcanvas", "flip: false").append(offCanvasDiv);
 
 // navbar search elements
@@ -38,41 +39,37 @@ var navbar = $("<nav uk-navbar>").addClass("uk-navbar-container uk-margin").appe
 
 // Pull info from the database and assign to arrays
 var recentVenueSearchTermsArray = [];
-console.log("recentVenueSearchTermsArray: ",recentVenueSearchTermsArray);
-
 var recentLocationSearchTermsArray = [];
     // Recent Event Venues Searched
 database.ref("Recent Event").on("child_added", function(childSnapshot) {
     var recentSearchTerm = childSnapshot.val().search;
-    recentVenueSearchTermsArray.unshift(recentSearchTerm);
+    recentSearchTerm = recentSearchTerm.toLowerCase();
+    if (recentVenueSearchTermsArray.indexOf(recentSearchTerm) === -1) {
+        recentVenueSearchTermsArray.unshift(recentSearchTerm);        
+        var recentSearchButton = $("<button>").addClass("uk-button uk-button-default uk-margin-small recent-search-event").attr("data-term", recentSearchTerm).text(recentSearchTerm);
+        offCanvasButtonDiv.append(recentSearchButton);
+    }
 });
     // Recent Locations Searched
 database.ref("Recent Location").on("child_added", function(childSnapshot) {
     var recentLocationSearchTerm = childSnapshot.val().search;
-    recentLocationSearchTermsArray.unshift(recentLocationSearchTerm);
+    recentLocationSearchTerm = recentLocationSearchTerm.toLowerCase();
+    if (recentLocationSearchTermsArray.indexOf(recentLocationSearchTerm) === -1) {
+        recentLocationSearchTermsArray.unshift(recentLocationSearchTerm);
+        var recentSearchButton = $("<button>").addClass("uk-button uk-button-default uk-margin-small recent-search-location").attr("data-term", recentLocationSearchTerm).text(recentLocationSearchTerm);
+        offCanvasButtonDiv.append(recentSearchButton);
+    }
 });
     // Recent Detailed Location
 database.ref("Recent Detailed Location").on("child_added", function(childSnapshot) {
     var search = childSnapshot.val().search;
     var detail = childSnapshot.val().detail;
     var searchWithDetail = search + ": " + detail;
-    recentLocationSearchTermsArray.unshift(searchWithDetail);
+    searchWithDetail = searchWithDetail.toLowerCase();
+    if (recentLocationSearchTermsArray.indexOf(searchWithDetail) === -1) {
+        recentLocationSearchTermsArray.unshift(searchWithDetail);
+    }
 });
-
-function buildButtons(arrayBuildingFrom) {
-    var rawArray = arrayBuildingFrom;
-    console.log("rawArray: ", rawArray);
-    console.log("uniqueArray: ", [...new Set(rawArray)]);
-    
-
-    // for (let index = 0; index < rawArray.length; index++) {
-    //     const arrayItem = rawArray[i].toLowerCase();
-    //     normalizedArray.push(arrayItem);
-    // }
-    // for (let index = 0; index < normalizedArray.length; index++) {
-        
-    // }
-}
 
 function searchEvent() {
     var eventSearchTerm = "";
@@ -96,6 +93,12 @@ function navbarEventSearch() {
         ticketMasterCall(eventSearchTerm);
     }
 }
+function eventBtn(btnValue) {
+    var eventSearchTerm = btnValue;
+    eventSearchTerm = eventSearchTerm.replace(/\s/g, "+");
+    $("#results").empty();
+    ticketMasterCall(eventSearchTerm);
+}
 function searchLocation() {
     var locationSearchTerm = "";
     if ($("#search-box").val() != "") {
@@ -118,6 +121,12 @@ function navbarLocationSearch() {
         yelpCall(locationSearchTerm);
     }
 }
+function locationBtn(btnValue) {    
+    var locationSearchTerm = btnValue;
+    locationSearchTerm = locationSearchTerm.replace(/\s/g, "+");
+    $("#results").empty();
+    yelpCall(locationSearchTerm);
+}
 function offcanvasYelpSearch() {
     var locationSearchTerm = $("#offcanvasLocationBar").val();
     var locationEventSearchTerm = $("#offcanvasLocationEventBar").val();
@@ -136,7 +145,6 @@ $(document).ready(function () {
     var searchDiv = $("<div>").append(searchInput, buttonsDiv);
     var landingDiv = $("<div>").addClass("uk-margin-xlarge-top").append(masthead, searchDiv);
     $("#root").append(landingDiv);
-    buildButtons(recentVenueSearchTermsArray)
     
     $("#event-button").on("click", searchEvent);
     $("#location-button").on("click", searchLocation);
@@ -144,4 +152,7 @@ $(document).ready(function () {
     $(document.body).on("click", "#navbar-location-button", navbarLocationSearch);
     $(document.body).on("click", "#navbar-event-button", navbarEventSearch);
     $(document.body).on("click", "#moreSpecificYelpCallButton", offcanvasYelpSearch);
+    $(document.body).on("click", ".recent-search-location", function(){locationBtn($(this).text());});
+    $(document.body).on("click", ".recent-search-event", function(){eventBtn($(this).text());});
+
 });
